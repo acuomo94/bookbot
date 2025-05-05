16a2a6112be13f24581202a7f6ec15b1418bbe30
acuomo <acuomo94@gmail.com>
2025-05-05 11:56:48 -0400

    Finished Project

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from stats import num_words, character_amount, sort_character_counts
+
+import sys
+from stats import (
+    num_words, 
+    character_amount, 
+    sort_character_counts
+)
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -7,18 +13,33 @@ def get_book_text(file_path):
 
     
 def main():
-    book_content = get_book_text("/home/acuomo/workspace/github.com/acuomo/bookbot/books/frankenstein.txt")
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    path_of_file = sys.argv[1]
+    book_content = get_book_text(path_of_file)
     count = num_words(book_content)
     char = character_amount(book_content)
     sorted_chars = sort_character_counts(char)
+    print_report(path_of_file, count, sorted_chars)
+    
+    
+    
+def print_report(path_of_file, count, sorted_chars):
+    
     print("=========BOOKBOT==========")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_of_file}...")
     print("---------Word Count---------")
     print(f"Found {count} total words")
     print("---------Character Count--------")
     
     for entry in sorted_chars:
-        print(f"{entry['char']}: {entry['num']}")
+        if entry["char"].isalpha():
+            print(f"{entry['char']}: {entry['num']}")
+
+           
     print("==========End==========")
 
     
